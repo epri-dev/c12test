aa572b5dc48d74412c5166d29beb7a46ecdeb1fa
Ed Beroset <beroset@ieee.org>
2021-04-30 17:45:45 -0400

    Refactor table interpretation into C12Meter.h and C12Meter.cpp

---
+++ src/C12Meter.h
@@ -0,0 +1,26 @@
+#ifndef C12METER_H
+#define C12METER_H
+#include "C12Tables.h"
+
+C12::Table MakeST0(const uint8_t *tabledata);
+C12::Table MakeST1();
+C12::Table MakeST2();
+C12::Table MakeST3();
+C12::Table MakeST5();
+C12::Table MakeST6();
+C12::Table MakeST20();
+C12::Table MakeST21();
+C12::Table MakeST40();
+C12::Table MakeST41();
+C12::Table MakeST50();
+C12::Table MakeST51();
+C12::Table MakeST52();
+C12::Table MakeST55();
+C12::Table MakeST56();
+C12::Table MakeST60();
+C12::Table MakeST61();
+C12::Table MakeST70();
+C12::Table MakeST71();
+C12::Table MakeST72();
+C12::Table MakeST73();
+#endif // C12METER_H
